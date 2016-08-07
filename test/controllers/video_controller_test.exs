1d0645b8e76855b3329a976fce253843c62b51a4
Nicolas Cavigneaux <nico@bounga.org>
2016-08-08 00:04:44 +0200

    Log in a user when needed for test
    Seed and log in a user conditionnaly using a tag

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,6 +1,17 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
 
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(build_conn(), :current_user, user)
+
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
   test "requires user authentification on all actions", %{conn: conn} do
     Enum.each([
       get(conn, video_path(conn, :new)),
@@ -14,5 +25,16 @@ defmodule Rumbl.VideoControllerTest do
       assert html_response(conn, 302)
       assert conn.halted
     end)
+  end
+
+  @tag login_as: "nico"
+  test "list all user's videos on index", %{conn: conn, user: user} do
+    user_video = insert_video(user, title: "Funny cats")
+    other_video = insert_video(insert_user(username: "other"), title: "Other video")
+
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
   end
 end
