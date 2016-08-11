c995982021dc54eb642edacd968af8aaf3d64f29
Nicolas Cavigneaux <nico@bounga.org>
2016-08-11 23:54:04 +0200

    Add tests for password validation and hashing

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -17,6 +17,22 @@ defmodule Rumbl.UserTest do
 
   test "changeset does not accept long username" do
     attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
-    assert {:username, {"should be at most %{count} character(s)", [count: 20]}} in errors_on(%User{}, attrs)
+    changeset = User.changeset(%User{}, attrs)
+    assert {:username, {"should be at most %{count} character(s)", [count: 20]}} in changeset.errors
+  end
+
+  test "registration_changeset password must be at least 6 chars long" do
+    attrs = Map.put(@valid_attrs, :password, "yeah")
+    changeset = User.registration_changeset(%User{}, attrs)
+    assert {:password, {"should be at least %{count} character(s)", [count: 6]}} in changeset.errors
+  end
+
+  test "registration_changeset with valid attributes hashes password" do
+    changeset = User.registration_changeset(%User{}, @valid_attrs)
+    %{password: pass, password_hash: pass_hash} = changeset.changes
+
+    assert changeset.valid?
+    assert pass_hash
+    assert Comeonin.Bcrypt.checkpw(pass, pass_hash)
   end
 end
