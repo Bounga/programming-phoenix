d0b8ab8bf129e63eaf36ab20df618fd6d4dfe007
Nicolas Cavigneaux <nico@bounga.org>
2016-08-15 15:35:42 +0200

    Add a slug to videos
    A slug is auto-generated when a video changeset contains change for
    the title attributes.

--- web/models/video.ex
+++ web/models/video.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -20,7 +21,22 @@ defmodule Rumbl.Video do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_fields, @optional_fields)
+    |> slugify_title()
     |> validate_required([:url, :title, :description])
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
