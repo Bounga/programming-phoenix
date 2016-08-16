4204781caf329292e13b6f388f18063b14256cd9
Nicolas Cavigneaux <nico@bounga.org>
2016-08-17 00:55:32 +0200

    Allow everyone to connect to our socket

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -21,4 +21,4 @@ import "phoenix_html";
 import socket from "./socket";
 import Video from "./video";
 
-Video.init(socket, document.getByElementById("video"));
+Video.init(socket, document.getElementById("video"));
