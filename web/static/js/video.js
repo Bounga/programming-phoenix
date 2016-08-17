a3add12e010b2d03984749a5d72c69f8b14412d9
Nicolas Cavigneaux <nico@bounga.org>
2016-08-17 18:26:37 +0200

    Ping sockets from channel every five seconds
    Add a timer to video channel that sends a ping to the sockets every
    five seconds. This commit also add the javascript code to listen to
    this ping and displays it with its count.

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -20,6 +20,8 @@ let Video = {
     const msgSubmit = document.getElementById("msg-submit");
     const vidChannel = socket.channel("videos:" + videoId);
 
+    vidChannel.on("ping", ({count}) => console.log("PING", count));
+
     vidChannel.join()
       .receive("ok", resp => console.log("joined the video channel", resp))
       .receive("error", reason => console.log("join failed", reason));
