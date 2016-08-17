74c0e52986fc5c404e5a9e2b0a31b74aeff54b59
Nicolas Cavigneaux <nico@bounga.org>
2016-08-17 22:38:20 +0200

    Remove socket global logging

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -3,13 +3,10 @@
 
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
-import {Socket} from "phoenix"
+import {Socket} from "phoenix";
 
 let socket = new Socket("/socket", {
-  params: {token: window.userToken},
-  logger: (kind, msg, data) =>  {
-    console.log(`${kind}: ${msg}`, data);
-  }
+  params: {token: window.userToken}
 });
 
 // When you connect, you'll often need to authenticate the client.
