c22a261b38d7f9ba8be9f3799ddd6f41d590b56c
Nicolas Cavigneaux <nico@bounga.org>
2018-01-08 00:09:52 +0100

    Contact info system on each message to reply with additional info using OTP

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -15,7 +15,7 @@ defmodule Rumbl do
       supervisor(Rumbl.InfoSys.Supervisor, []),
       # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
       # worker(Rumbl.Worker, [arg1, arg2, arg3]),
-      worker(Rumbl.Counter, [5])
+      #worker(Rumbl.Counter, [5])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
